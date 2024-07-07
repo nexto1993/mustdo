c696a08ee10078b47d88eee911536d0ca5db0c39
nexto1993 <nexto.conan@gmail.com>
2024-07-07 23:00:15 +0300

    first commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using API.Entities;
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController(ApplicationDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var users = await context.Appusers.ToListAsync();
+            return Ok(users);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var user = await context.Appusers.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(user);
+        }
+    }
+    
+    
+}
